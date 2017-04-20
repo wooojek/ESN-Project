08f16d3023a05e47a1ae82e72e73940400b4af78
Michal Z <yokomura123@gmail.com>
2017-04-20 11:35:01 +0200

    prepared files to work on them

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+//Konfiguracja Webpack
+module.exports	=	{
+		entry:	"./js/app.jsx",
+		output:	{	filename:	"./js/out.js"	},
+		watch:	true,
+		module:	{
+				loaders:	[
+						{
+								test:	/\.jsx$/,		exclude:	/node_modules/,
+								loader:	'babel-loader',
+								query:	{	presets:	['es2015',	'stage-2', 'react']	}
+						}
+				]
+		}
+}
