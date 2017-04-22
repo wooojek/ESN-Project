aa4ab23168d541b34ecb4c3deacfb3be9fc127ed
Michał <yokomura123@gmail.com>
2017-04-22 12:36:37 +0200

    created example data files

---
+++ js/data/sites.js
@@ -0,0 +1,62 @@
+const data = {
+    sites: [
+            {
+                "_id": "58fb2576cc02bf7f6aefc7f4",
+                "index": 0,
+                "guid": "8b7f3b41-ece6-44b7-9637-9acf452d4a06",
+                "url": "http://google.com",
+                "name": "TROPOLI",
+                "about": "Do magna occaecat aliquip pariatur sit tempor cillum aliquip. Ullamco exercitation exercitation velit sint. Esse eiusmod ut pariatur culpa ea nulla. Nisi incididunt cupidatat deserunt elit consectetur id dolore est occaecat. Anim aute elit enim deserunt consectetur labore et mollit cillum id anim proident pariatur."
+            },
+            {
+                "_id": "58fb2576fd5d6aaa4515a5d9",
+                "index": 1,
+                "guid": "57fc24a3-9bab-4b45-b6ee-0c49834feff0",
+                "url": "http://google.com",
+                "name": "GEOFORM",
+                "about": "Est nostrud tempor qui adipisicing proident aute reprehenderit adipisicing mollit cillum anim. Deserunt deserunt enim esse deserunt sint labore Lorem proident eu non cillum officia. In irure deserunt magna ut mollit sit aliqua. Elit ullamco aliquip irure ad non nostrud sint. Eiusmod minim labore amet culpa veniam est."
+            },
+            {
+                "_id": "58fb257602d3dc45eb440588",
+                "index": 2,
+                "guid": "12a834a7-e0ca-4571-98ea-aabdc337094b",
+                "url": "http://google.com",
+                "name": "BLEEKO",
+                "about": "Consequat dolor proident nostrud minim ipsum commodo nisi exercitation duis tempor non cupidatat exercitation. Voluptate tempor dolor mollit enim. Nulla occaecat voluptate amet id eiusmod amet. Voluptate aliquip occaecat aute labore incididunt dolore ullamco. Do officia laborum commodo pariatur sint incididunt. Ad minim incididunt nulla cupidatat aliqua reprehenderit deserunt elit et fugiat. Proident labore eiusmod occaecat eiusmod qui ea excepteur ea amet."
+            },
+            {
+                "_id": "58fb2576757ea88071ddcf90",
+                "index": 3,
+                "guid": "ebf1d53d-3ee8-4e82-b81f-5ade8c35ece9",
+                "url": "http://google.com",
+                "name": "INTRAWEAR",
+                "about": "Irure id aliqua consectetur excepteur id anim laboris sunt labore adipisicing consequat. Aliquip Lorem labore ullamco voluptate commodo consectetur ex laboris amet. Aliqua irure occaecat ex do reprehenderit Lorem ut magna. Enim incididunt ea ea voluptate aliqua do culpa laboris eiusmod irure minim id proident eiusmod. Minim excepteur nulla elit ipsum incididunt occaecat ea pariatur anim cupidatat reprehenderit laborum duis non. Anim anim enim cillum cupidatat velit est cillum. Elit reprehenderit fugiat tempor aute deserunt mollit sint tempor ex ut quis."
+            },
+            {
+                "_id": "58fb2576ed6720cdf223c59a",
+                "index": 4,
+                "guid": "93ca9b42-9bb3-4935-ae5f-b938b60abd14",
+                "url": "http://google.com",
+                "name": "INSURETY",
+                "about": "Enim nostrud dolore in voluptate do exercitation id sit ut. Veniam excepteur qui non laborum deserunt sunt. Dolore tempor reprehenderit incididunt ut eu mollit ex veniam veniam aute dolor eu est. Do ut esse laborum Lorem. Elit proident do consequat amet irure irure ad occaecat velit sit ad Lorem quis dolore. Aliqua labore deserunt fugiat aliqua ad eiusmod mollit pariatur aute velit incididunt commodo cillum. Minim duis aliquip velit dolor."
+            },
+            {
+                "_id": "58fb2576d378f868152128c0",
+                "index": 5,
+                "guid": "638e291f-9121-4b78-98d5-3eada221bd61",
+                "url": "http://google.com",
+                "name": "MANTRIX",
+                "about": "Exercitation consectetur nostrud incididunt occaecat incididunt amet ullamco laborum. Est nisi consequat sunt anim ullamco sunt adipisicing. Laboris elit fugiat labore ullamco elit tempor magna commodo aliqua tempor occaecat tempor velit. Laborum nulla id occaecat dolore et amet esse sunt cillum magna."
+            },
+            {
+                "_id": "58fb25767fc3227c1503c8c2",
+                "index": 6,
+                "guid": "e56f74e2-e168-4d17-ba06-f8518376688d",
+                "url": "http://google.com",
+                "name": "ENDICIL",
+                "about": "Et ad nostrud irure adipisicing enim cupidatat do commodo culpa. Dolore nulla laborum tempor laborum magna deserunt culpa Lorem laboris. Eu eiusmod minim ullamco do ullamco laboris. Cupidatat eu cillum tempor ullamco in dolore in proident. Sunt enim laboris non duis et culpa aute minim pariatur. Quis fugiat anim quis ea ut et quis anim proident."
+            }
+    ]
+}
+
+export default data;
--- js/sites.jsx
+++ js/sites.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
 
+import data from './data/sites.js';
+
 class Sites extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            sitesListData: data.sites,
+        }
+        console.log(this.state.sitesListData);
+    }
+
+    handleTheData = () => {
+        const sitesList = this.state.sitesListData.map((element, index) => {
+            return <li><a href={element.url}>{element.name}</a><p>{element.about}</p></li>;
+        });
+        return sitesList;
+    }
+
     render() {
+        const sitesList = this.handleTheData();
         return <div id='sites'>
             <h1>Sites</h1>
             <ul>
-                <li>
-                    <a href="http://google.com/" target="_blank">Google.com</a>
-                    <p>Popularna wyszukiwarka Google.com</p>
-                </li>
-                <li>
-                    <a href="http://google.com/" target="_blank">Google.com</a>
-                    <p></p>
-                </li>
-                <li>
-                    <a href="http://google.com/" target="_blank">Google.com</a>
-                    <p></p>
-                </li>
+                {sitesList}
             </ul>
         </div>;
     }
