74e9ff0aa5acd7b903c9e2e2eb06e9c5b8ff3e59
Michał <yokomura123@gmail.com>
2017-04-23 22:01:07 +0200

    finished news component

---
+++ js/data/newsFeed.js
@@ -0,0 +1,167 @@
+const newsData = {
+    news: [
+  {
+    "greeting": "Hello, Daugherty! You have 7 unread messages.",
+    "tags": [
+      "cillum",
+      "est",
+      "culpa",
+      "ex",
+      "nisi"
+    ],
+    "registered": "Wednesday, October 19, 2016 6:40 AM",
+    "about": "Reprehenderit dolore veniam esse magna exercitation reprehenderit ad proident. Aute cillum pariatur ullamco excepteur ad amet dolore in. Anim excepteur dolore sunt duis mollit ad sit exercitation. Lorem id ad irure ipsum occaecat aliquip excepteur adipisicing minim ut incididunt.",
+    "phone": "+1 (982) 520-3308",
+    "email": "daugherty.buckner@undefined.org",
+    "name": {
+      "last": "Buckner",
+      "first": "Daugherty"
+    },
+    "isDropped": false,
+    "picture": "./img/ic_face_black_24px.svg",
+    "guid": "d1bac463-7fa2-44bc-ab8f-cb7efcda01bf",
+    "index": 0,
+    "_id": "58fd058259215b8c18e56130"
+  },
+  {
+    "greeting": "Hello, Randolph! You have 5 unread messages.",
+    "tags": [
+      "pariatur",
+      "dolor",
+      "et",
+      "ea",
+      "esse"
+    ],
+    "registered": "Friday, September 16, 2016 2:01 PM",
+    "about": "Sit mollit velit incididunt eiusmod amet elit est aliqua incididunt amet elit enim culpa mollit. Id laborum sint et sunt tempor. Exercitation eu Lorem laboris ad. Laboris fugiat ut sunt consequat magna aliquip eu excepteur pariatur ullamco Lorem proident laborum.",
+    "phone": "+1 (865) 480-3379",
+    "email": "randolph.reynolds@undefined.me",
+    "name": {
+      "last": "Reynolds",
+      "first": "Randolph"
+    },
+    "isDropped": false,
+    "picture": "./img/ic_face_black_24px.svg",
+    "guid": "04f37c5e-64a5-460c-ac0a-79ae2de93a48",
+    "index": 1,
+    "_id": "58fd0582d05250945c0469ed"
+  },
+  {
+    "greeting": "Hello, Henry! You have 5 unread messages.",
+    "tags": [
+      "eu",
+      "sunt",
+      "tempor",
+      "amet",
+      "proident"
+    ],
+    "registered": "Thursday, October 16, 2014 12:47 PM",
+    "about": "Sint nisi amet dolore enim consectetur minim. Magna amet qui sit Lorem officia ad minim nulla. Nostrud incididunt ut in exercitation irure nulla ipsum. Et laborum voluptate minim veniam nostrud id laborum ut culpa deserunt cupidatat. Minim qui magna sint nisi eu ex proident velit culpa do sunt nisi. Adipisicing sint anim sint est. Tempor amet Lorem in cupidatat.",
+    "phone": "+1 (886) 518-3966",
+    "email": "henry.schneider@undefined.com",
+    "name": {
+      "last": "Schneider",
+      "first": "Henry"
+    },
+    "isDropped": false,
+    "picture": "./img/ic_face_black_24px.svg",
+    "guid": "01212ac0-e1b0-46fc-9a68-f8647bdcaebf",
+    "index": 2,
+    "_id": "58fd058286d96881895d3752"
+  },
+  {
+    "greeting": "Hello, Lowe! You have 7 unread messages.",
+    "tags": [
+      "reprehenderit",
+      "irure",
+      "minim",
+      "nulla",
+      "occaecat"
+    ],
+    "registered": "Tuesday, July 19, 2016 4:24 AM",
+    "about": "Ea magna minim labore non quis qui tempor mollit duis non dolore. Non laborum culpa cupidatat laboris non labore sunt veniam cillum. Id labore veniam ad quis ipsum nulla aute anim Lorem. Proident esse nulla est consequat in nostrud occaecat do sint enim in cillum. Cupidatat Lorem ea eiusmod amet culpa nostrud amet commodo.",
+    "phone": "+1 (875) 528-3297",
+    "email": "lowe.moon@undefined.ca",
+    "name": {
+      "last": "Moon",
+      "first": "Lowe"
+    },
+    "isDropped": false,
+    "picture": "./img/ic_face_black_24px.svg",
+    "guid": "9b3a5cdc-d0a2-4878-b025-4e61a334595f",
+    "index": 3,
+    "_id": "58fd058290ca43ec8d53a092"
+  },
+  {
+    "greeting": "Hello, Kathrine! You have 5 unread messages.",
+    "tags": [
+      "consectetur",
+      "cillum",
+      "eu",
+      "amet",
+      "in"
+    ],
+    "registered": "Friday, August 8, 2014 6:23 PM",
+    "about": "Do minim eiusmod Lorem dolor cillum laborum nostrud adipisicing. Cillum cillum ullamco in ad pariatur sunt labore qui amet nisi velit aliquip. Non eiusmod irure velit enim est proident. Minim sit quis adipisicing labore et amet cupidatat elit quis culpa laborum eiusmod. Adipisicing minim enim reprehenderit ex excepteur. In dolore commodo excepteur cillum sunt.",
+    "phone": "+1 (858) 592-3325",
+    "email": "kathrine.banks@undefined.name",
+    "name": {
+      "last": "Banks",
+      "first": "Kathrine"
+    },
+    "isDropped": false,
+    "picture": "./img/ic_face_black_24px.svg",
+    "guid": "fc90308b-f47d-4f3e-8552-f4af00b2acf1",
+    "index": 4,
+    "_id": "58fd0583496feb64d03f1f1d"
+  },
+  {
+    "greeting": "Hello, Jayne! You have 7 unread messages.",
+    "tags": [
+      "non",
+      "elit",
+      "labore",
+      "irure",
+      "consequat"
+    ],
+    "registered": "Monday, February 29, 2016 7:40 AM",
+    "about": "Id duis nisi ullamco sit ad irure aliquip qui duis dolor id ex ut. Pariatur exercitation est Lorem cupidatat labore nisi commodo velit. Enim cupidatat sit elit excepteur. Do et veniam aute ullamco reprehenderit et voluptate commodo reprehenderit laborum ea. Esse id ut labore ut enim velit officia ipsum quis voluptate laborum. Fugiat ex minim incididunt enim ex consequat voluptate.",
+    "phone": "+1 (804) 469-3287",
+    "email": "jayne.holt@undefined.us",
+    "name": {
+      "last": "Holt",
+      "first": "Jayne"
+    },
+    "isDropped": false,
+    "picture": "./img/ic_face_black_24px.svg",
+    "guid": "610145a9-5703-49a4-8b70-a23c15a2dad8",
+    "index": 5,
+    "_id": "58fd0583fe185f035a759df3"
+  },
+  {
+    "greeting": "Hello, Susan! You have 10 unread messages.",
+    "tags": [
+      "consequat",
+      "laboris",
+      "esse",
+      "sint",
+      "magna"
+    ],
+    "registered": "Friday, August 19, 2016 1:13 PM",
+    "about": "Labore ullamco sint proident ex Lorem commodo. Fugiat commodo incididunt deserunt ad. Eiusmod sit labore exercitation ex tempor tempor proident. Duis cupidatat eu ad voluptate ut laborum. Ipsum cupidatat eiusmod est laboris sit sint esse quis duis.",
+    "phone": "+1 (857) 503-2540",
+    "email": "susan.hewitt@undefined.io",
+    "name": {
+      "last": "Hewitt",
+      "first": "Susan"
+    },
+    "isDropped": false,
+    "picture": "./img/ic_face_black_24px.svg",
+    "guid": "0be8df4c-eb71-4b5b-b8da-236e1965d6ea",
+    "index": 6,
+    "_id": "58fd05839a816459181f116d"
+  }
+]
+}
+
+export default newsData;
--- js/newsFeed.jsx
+++ js/newsFeed.jsx
@@ -1,8 +1,74 @@
 import React from 'react';
 
-class NewsFeed extends React.Component {
+import newsData from './data/newsFeed.js';
+
+class NewsFeedItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            element: this.props.data,
+        }
+    }
+
+    handleDropClick = (event) => {
+        
+        let dropMeDiv = '';
+        const itemElement = this.state.element.isDropped;
+
+        if(event.target.id !== this.state.element._id) {
+            dropMeDiv = event.target.parentNode.nextSibling;
+        } else {
+            dropMeDiv = event.target.nextSibling;
+        }
+
+        itemElement ? dropMeDiv.classList.add('display') : dropMeDiv.classList.remove('display');
+
+        let newElement = this.state.element;
+        newElement.isDropped = !this.state.element.isDropped;
+
+        this.setState({
+            element: newElement,
+        });
+    }
+
     render() {
-        return <div id='newsFeed'>NewsFeed</div>;
+        return <li>
+                    <div>
+                        <span>{this.state.element.greeting}</span>
+                        <button id={this.state.element._id} onClick={event => this.handleDropClick(event)}>{this.state.element.isDropped ? 'Zwiń' : 'Rozwiń'} <img src={this.state.element.isDropped ? './img/ic_keyboard_arrow_up_black_24px.svg' : './img/ic_keyboard_arrow_down_black_24px.svg'}/></button>
+                        <div className='display'>
+                            <img src={this.state.element.picture}/>
+                            <span><h3>{this.state.element.name.first} {this.state.element.name.last}</h3> {this.state.element.email}</span>
+                            <p>
+                                {this.state.element.about}
+                            </p>
+                        </div>
+                    </div>
+                </li>;
+    }
+}
+
+class NewsFeed extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            news: newsData.news,
+        }
+    }
+
+    render() {
+        const news = this.state.news.map(element => {
+            return <NewsFeedItem key={element._id} data={element}/>
+        });
+
+        return <div id='newsFeed'>
+            <h2>NewsFeed</h2>
+            <ul>
+                {news}
+            </ul>
+        </div>;
     }
 }
 
