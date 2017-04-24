c3615677413673a5d057cb84d8203ab92b35b67e
Michał <yokomura123@gmail.com>
2017-04-24 21:35:33 +0200

    finished messages component

---
+++ js/data/messages.js
@@ -0,0 +1,96 @@
+const messagesData = {
+    data: [
+        {
+            "_id": "58fe4e8624def63bc071f0c5",
+            "_id2": "58fe4e8694ef7bbcc86e1706",
+            "index": 0,
+            "guid": "0dcb89b4-b4b6-45b2-9426-e58862db5306",
+            "picture": "./img/ic_face_black_24px.svg",
+            "isOpen": false,
+            "name": {
+            "first": "Flora",
+            "last": "Atkinson"
+            },
+            "name2": {
+            "first": "Tia",
+            "last": "Reed"
+            },
+            "about": "Eu deserunt exercitation voluptate occaecat quis laborum veniam ut. Labore irure consequat velit cillum mollit ad do. Anim aliqua ullamco exercitation sit mollit sint deserunt. In do labore qui cillum exercitation qui labore occaecat. Culpa adipisicing eu enim aliqua tempor nostrud sit magna ipsum consectetur eiusmod irure eu. Anim cupidatat irure aliquip anim mollit adipisicing deserunt.",
+            "registered": "Friday, March 27, 2015 5:56 PM"
+        },
+        {
+            "_id": "58fe4e8717e1aab23547d1ad",
+            "_id2": "58fe4e875219ef3a29bd9571",
+            "index": 1,
+            "guid": "21744e89-206f-474b-9b7f-20d709008f93",
+            "picture": "./img/ic_face_black_24px.svg",
+            "isOpen": false,
+            "name": {
+            "first": "Flora",
+            "last": "Atkinson"
+            },
+            "name2": {
+            "first": "Gladys",
+            "last": "Williamson"
+            },
+            "about": "Labore nisi adipisicing aute ipsum. Dolore ex ea amet tempor. Officia nisi aute culpa incididunt tempor aliqua incididunt ipsum. Dolor sint duis in cillum id consequat sit mollit aliqua ad do qui elit.",
+            "registered": "Saturday, August 6, 2016 8:53 AM"
+        },
+        {
+            "_id": "58fe4e876421be4a676d33a4",
+            "_id2": "58fe4e87f47a60ba93e49118",
+            "index": 2,
+            "guid": "aac0bbff-0c41-4f10-8887-53fbcd2f64f2",
+            "picture": "./img/ic_face_black_24px.svg",
+            "isOpen": false,
+            "name": {
+            "first": "Flora",
+            "last": "Atkinson"
+            },
+            "name2": {
+            "first": "Knox",
+            "last": "Battle"
+            },
+            "about": "Officia dolor deserunt excepteur reprehenderit consequat veniam sunt minim. Tempor ex consequat officia pariatur cupidatat sunt magna magna sunt duis. Et nostrud officia exercitation officia commodo ea Lorem nisi. Cupidatat anim ut culpa veniam pariatur nulla reprehenderit qui magna. Eu excepteur ipsum consectetur laboris dolor minim proident. Deserunt eu amet magna proident reprehenderit consequat mollit elit pariatur. Id ea eu non qui consectetur tempor veniam occaecat ex consequat in aliqua.",
+            "registered": "Friday, February 6, 2015 5:20 AM"
+        },
+        {
+            "_id": "58fe4e87fba5d96cac96f65c",
+            "_id2": "58fe4e8764b5c56cad172e67",
+            "index": 3,
+            "guid": "f91829af-763d-42ac-9cc3-d76481753c39",
+            "picture": "./img/ic_face_black_24px.svg",
+            "isOpen": false,
+            "name": {
+            "first": "Flora",
+            "last": "Atkinson"
+            },
+            "name2": {
+            "first": "Lilia",
+            "last": "Kinney"
+            },
+            "about": "Ea laborum veniam anim ad labore minim ex quis. Reprehenderit eiusmod ea adipisicing eiusmod duis ea duis ullamco culpa dolore. Dolor quis nisi ea veniam sunt et cupidatat fugiat veniam magna consectetur est proident qui. Tempor anim ea reprehenderit eu dolor anim consequat voluptate reprehenderit. Labore culpa adipisicing ullamco qui mollit et consequat commodo culpa Lorem voluptate irure.",
+            "registered": "Saturday, February 21, 2015 7:33 AM"
+        },
+        {
+            "_id": "58fe4e873f45b620abcd484b",
+            "_id2": "58fe4e87fe51b61ecfc189ea",
+            "index": 4,
+            "guid": "7cb5051c-db0e-4b44-ae77-eba661896e97",
+            "picture": "./img/ic_face_black_24px.svg",
+            "isOpen": false,
+            "name": {
+            "first": "Flora",
+            "last": "Atkinson"
+            },
+            "name2": {
+            "first": "Katy",
+            "last": "Miles"
+            },
+            "about": "Sint voluptate voluptate ex incididunt. Dolore quis sint fugiat duis quis cupidatat ut fugiat laborum consequat deserunt duis et minim. Ex do deserunt irure duis sit commodo irure consequat consequat aliquip tempor qui esse.",
+            "registered": "Monday, January 27, 2014 9:50 PM"
+        }
+    ]
+}
+
+export default messagesData;
--- js/messages.jsx
+++ js/messages.jsx
@@ -1,8 +1,76 @@
 import React from 'react';
 
-class Messages extends React.Component {
+import messagesData from './data/messages.js';
+
+class MessagesItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: this.props.elementData,
+        }
+    }
+
+    handleOnClick = (event) => {
+        
+        let dropMeDiv = '';
+        const itemElement = this.state.data.isOpen;
+
+        if(event.target.id !== this.state.data._id2) {
+            dropMeDiv = event.target.parentNode.nextSibling;
+        } else {
+            dropMeDiv = event.target.nextSibling;
+        }
+
+        itemElement ? dropMeDiv.classList.add('display') : dropMeDiv.classList.remove('display');
+
+        let newElement = this.state.data;
+        newElement.isOpen = !this.state.data.isOpen;
+
+        this.setState({
+            data: newElement,
+        });
+    }
+
     render() {
-        return <div id='messages'>Messages</div>;
+        return <li key={this.state.data._id}>
+                <button id={this.state.data._id2} onClick={e => this.handleOnClick(e)}>
+                    <h3><img src={this.state.data.picture} />{this.state.data.name2.first} {this.state.data.name2.last}</h3>
+                </button>
+                <div className="display">
+                    <p>Hello {this.state.data.name.first}! {this.state.data.about}</p>
+                    <p>Wysłano: {this.state.data.registered}</p>
+                </div>
+            </li>
+    }
+}
+
+class Messages extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: [],
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            data: messagesData.data,
+        });
+    }
+
+    render() {
+        const messages = this.state.data.map(element => {
+            return <MessagesItem elementData={element} />;
+        });
+
+        return <div id='messages'>
+            <h2>Messages</h2>
+            <ul>
+                {messages}
+            </ul>
+        </div>;
     }
 }
 
