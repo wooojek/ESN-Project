333c605c5a059f2b947bee70a7284538975c415f
Michał <yokomura123@gmail.com>
2017-05-09 14:36:28 +0200

    added redux and axios to dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,10 @@ module.exports	=	{
 						{
 								test:	/\.jsx$/,		exclude:	/node_modules/,
 								loader:	'babel-loader',
-								query:	{	presets:	['es2015',	'stage-2', 'react']	}
+								query:	{	
+											presets:	['es2015',	'stage-2', 'react'],
+											plugins: ["transform-decorators-legacy"]
+										}
 						},
 						{
 								test: /\.scss$/,
