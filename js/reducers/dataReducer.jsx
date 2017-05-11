fdad1b4849a7724b323069084859fb8709a69c43
Michał <yokomura123@gmail.com>
2017-05-11 17:16:11 +0200

    removed unused dispatches

--- js/reducers/dataReducer.jsx
+++ js/reducers/dataReducer.jsx
@@ -9,30 +9,6 @@ const dataReducer = (state=initialState, action) => {
             state = {...state, data: action.payload.data, user: action.payload.users[userId],  fetching: false };
             break;
         }
-        case 'FETCH_DATA_NEWS': {
-            state = {...state, 
-                        data: { news: action.payload },
-                    };
-            break;
-        }
-        case 'FETCH_DATA_GROUPS': {
-            state = {...state, 
-                        data: { groups: action.payload },
-                    };
-            break;
-        }
-        case 'FETCH_DATA_KNOWLEDGE': {
-            state = {...state, 
-                        data: { knowledge: action.payload },
-                    };
-            break;
-        }
-        case 'FETCH_DATA_SITES': {
-            state = {...state, 
-                        data: { sites: action.payload },
-                    };
-            break;
-        }
         case 'E': {
             throw new Error("aaaaa!", action.payload);
         }
